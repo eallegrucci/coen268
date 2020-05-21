aeaaeda68b50334f04e31f7827870bb8f92fd9b2
Emma Allegrucci <emmallegrucci@gmail.com>
2020-05-20 23:37:33 -0700

    images added, need to add search bar and EditText for search feature

--- app/src/main/java/com/example/coen268/RestaurantsAdapter.java
+++ app/src/main/java/com/example/coen268/RestaurantsAdapter.java
@@ -12,6 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
@@ -63,7 +66,9 @@ public class RestaurantsAdapter extends RecyclerView.Adapter<RestaurantsAdapter.
             mDistance.setText(r.distanceInMiles() + "");
             mPrice.setText(r.price+ "");
             mReviewCount.setText(r.review_count + " Reviews");
-            //Glide.with(itemView).load(r.image_url).into(mImage);
+            Glide.with(itemView).load(r.image_url).transform(
+                    new CenterCrop(), new RoundedCorners(20)
+            ).into(mImage);
         }
     }
 
