2a108580529641501e7e2221cc20753ec2828cc9
Emma Allegrucci <emmallegrucci@gmail.com>
2020-05-22 17:25:38 -0700

    Back button added to RestaurantActivity

--- app/src/main/java/com/example/coen268/RestaurantActivity.java
+++ app/src/main/java/com/example/coen268/RestaurantActivity.java
@@ -26,6 +26,7 @@ public class RestaurantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent i = getIntent();
         getInfo(i);
