659ac1e702dfb406d9fbfbd5cc26d33daafe74ff
Emma Allegrucci <emmallegrucci@gmail.com>
2020-06-03 17:23:54 -0700

    account activity add, bottom navbar added and fully fucntional

--- app/src/main/java/com/example/coen268/Account.java
+++ app/src/main/java/com/example/coen268/Account.java
@@ -4,17 +4,42 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Account extends Fragment {
+
+    private Button logout;
+    private TextView username;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_account, container, false);
+
+        logout = v.findViewById(R.id.btnLogOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        username = v.findViewById(R.id.tvUserName);
+
+        //TODO: Add user's name below
+        String name = "";
+        username.setText("Hello, " + name);
 
         return v;
     }
--- app/src/main/java/com/example/coen268/MainActivity.java
+++ app/src/main/java/com/example/coen268/MainActivity.java
@@ -20,39 +20,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         navBar = findViewById(R.id.bottomNavigation);
-        navBar.setOnNavigationItemSelectedListener(navListener);
-
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFrag = null;
+                switch (item.getItemId()) {
+                    case R.id.homeItem:
+                        Log.i("onNavigationItemSelected", "search fragment selected");
+                        selectedFrag = new SearchActivity();
+                        break;
+                    case R.id.accountItem:
+                        Log.i("onNavigationItemSelected", "account fragment selected");
+                        selectedFrag = new Account();
+                        break;
+                    case R.id.myReservationsItem:
+                        Log.i("onNavigationItemSelected", "reservation fragment selected");
+                        selectedFrag = new ReservationFragment();
+                        break;
+                }
+                if (selectedFrag != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFrag).addToBackStack(null).commit();
+                    return true;
+                } else {
+                    Log.i("onNavigationItemSelected", "selected frag is null");
+                    return false;
+                }
+            }
+        });
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new SearchActivity()).addToBackStack(null).commit();
     }
-
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFrag = new SearchActivity();
-                    switch (item.getItemId()) {
-                        case R.id.search_fragment:
-                            Log.i("onNavigationItemSelected", "search fragment selected");
-                            selectedFrag = new SearchActivity();
-                            break;
-                        case R.id.account_fragment:
-                            Log.i("onNavigationItemSelected", "account fragment selected");
-                            selectedFrag = new SearchActivity();
-                            break;
-                        case R.id.reservation_fragment:
-                            Log.i("onNavigationItemSelected", "reservation fragment selected");
-                            selectedFrag = new SearchActivity();
-                            break;
-                    }
-                    if (selectedFrag != null) {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                selectedFrag).addToBackStack(null).commit();
-                        return true;
-                    }
-                    return false;
-                }
-            };
+//
+//
+//    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+//            new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                @Override
+//                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                    Fragment selectedFrag = new SearchActivity();
+//                    switch (item.getItemId()) {
+//                        case R.id.search_fragment:
+//                            Log.i("onNavigationItemSelected", "search fragment selected");
+//                            selectedFrag = new SearchActivity();
+//                            break;
+//                        case R.id.account_fragment:
+//                            Log.i("onNavigationItemSelected", "account fragment selected");
+//                            selectedFrag = new SearchActivity();
+//                            break;
+//                        case R.id.reservation_fragment:
+//                            Log.i("onNavigationItemSelected", "reservation fragment selected");
+//                            selectedFrag = new SearchActivity();
+//                            break;
+//                    }
+//                    if (selectedFrag != null) {
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                                selectedFrag).addToBackStack(null).commit();
+//                        return true;
+//                    }
+//                    return false;
+//                }
+//            };
 }
